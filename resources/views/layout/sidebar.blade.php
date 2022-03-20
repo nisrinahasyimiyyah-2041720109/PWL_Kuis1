5519ddb3e906e139416ac835422e52a4222f2477
nisrinahasyimiyyah-2041720109 <hasyimiyyahnisrina@gmail.com>
2022-03-20 15:16:39 +0700

    Mengubah Tampilan Home

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,9 +1,9 @@
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-light-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{ url('/home') }}" class="brand-link">
-            <img src="{{asset('template')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <img src="{{ asset('style/img/sayur-buah.jpg') }}" alt="VegeFruit Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">VegeFruit</span>
         </a>
 
         <!-- Sidebar -->
